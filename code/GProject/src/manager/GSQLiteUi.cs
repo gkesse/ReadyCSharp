ba774bc936618d0cb207923632cc29ff92c749c1
Gerard KESSE <tiakagerard@hotmail.com>
2020-12-09 12:34:29 +0100

    Initial Commit

--- code/GProject/src/manager/GSQLiteUi.cs
+++ code/GProject/src/manager/GSQLiteUi.cs
@@ -37,6 +37,7 @@ public sealed class GSQLiteUi {
             //
             else if(G_STATE == "S_SHOW_TABLES") {run_SHOW_TABLES(args);}
             else if(G_STATE == "S_CONFIG_SHARP_SHOW_DATA") {run_CONFIG_SHARP_SHOW_DATA(args);}
+            else if(G_STATE == "S_CONFIG_SHARP_SHOW_SCHEMA") {run_CONFIG_SHARP_SHOW_SCHEMA(args);}
             //
             else if(G_STATE == "S_SAVE") {run_SAVE(args);}
             else if(G_STATE == "S_LOAD") {run_LOAD(args);}
@@ -64,8 +65,9 @@ public sealed class GSQLiteUi {
     public void run_METHOD(string[] args) {
         Console.Write("SQLITE_ADMIN :\n");
         Console.Write("\t{0,-2} : {1}\n", "1", "afficher les tables");
-        Console.Write("\t{0,-2} : {1}\n", "2", "creer la table CONFIG_SHARP");
-        Console.Write("\t{0,-2} : {1}\n", "3", "supprimer la table CONFIG_SHARP");
+        Console.Write("\t{0,-2} : {1}\n", "2", "afficher les donnees CONFIG_SHARP");
+        Console.Write("\t{0,-2} : {1}\n", "3", "afficher le schema CONFIG_SHARP");
+        Console.Write("\t{0,-2} : {1}\n", "4", "supprimer la table CONFIG_SHARP");
         Console.Write("\n");
         G_STATE = "S_CHOICE";
     }
@@ -81,13 +83,15 @@ public sealed class GSQLiteUi {
         //
         else if(lAnswer == "1") {G_STATE = "S_SHOW_TABLES"; GConfig.Instance().setData("G_SQLITE_ID", lAnswer);} 
         else if(lAnswer == "2") {G_STATE = "S_CONFIG_SHARP_SHOW_DATA"; GConfig.Instance().setData("G_SQLITE_ID", lAnswer);} 
+        else if(lAnswer == "3") {G_STATE = "S_CONFIG_SHARP_SHOW_SCHEMA"; GConfig.Instance().setData("G_SQLITE_ID", lAnswer);} 
         //
     }
     //===============================================
     public void run_SHOW_TABLES(string[] args) {
         Console.Write("\n");
         string lQuery = String.Format(@"
-        select * from sqlite_master
+        select name from sqlite_master
+        where type = 'table'
         ");
         GSQLite.Instance().queryShow(lQuery);
         G_STATE = "S_SAVE";
@@ -97,6 +101,17 @@ public sealed class GSQLiteUi {
         Console.Write("\n");
         string lQuery = String.Format(@"
         select * from config_data
+        ");
+        GSQLite.Instance().queryShow(lQuery);
+        G_STATE = "S_SAVE";
+    }
+    //===============================================
+    public void run_CONFIG_SHARP_SHOW_SCHEMA(string[] args) {
+        Console.Write("\n");
+        string lQuery = String.Format(@"
+        select name from sqlite_master
+        where type = 'table'
+        and name = 'config_data'
         ");
         GSQLite.Instance().queryShow(lQuery);
         G_STATE = "S_SAVE";
