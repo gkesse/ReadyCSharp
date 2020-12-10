8ca4623fbd2d72c794f64b979325a4970ddf4056
Gerard KESSE <tiakagerard@hotmail.com>
2020-12-10 22:44:04 +0100

    Initial Commit

--- code/GProject/src/manager/GOpenCV.cs
+++ code/GProject/src/manager/GOpenCV.cs
@@ -1,7 +1,7 @@
 //===============================================
 using System;
-using Emgu;
 using Emgu.CV;
+using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;  
 //===============================================
 public sealed class GOpenCV {
@@ -29,9 +29,24 @@ public sealed class GOpenCV {
     // method
     //===============================================
     public void run(string[] args) {
-            CvInvoke.CheckLibraryLoaded();
+CvInvoke.CheckLibraryLoaded();
             String win1 = "Test Window"; //The name of the window
             CvInvoke.NamedWindow(win1); //Create the window using the specific name
+
+            Mat img = new Mat(200, 400, DepthType.Cv8U, 3); //Create a 3 channel image of 400x200
+            img.SetTo(new Bgr(255, 0, 0).MCvScalar); // set it to Blue color
+
+            //Draw "Hello, world." on the image using the specific font
+            CvInvoke.PutText(
+               img,
+               "Hello, world",
+               new System.Drawing.Point(10, 80),
+               FontFace.HersheyComplex,
+               1.0,
+               new Bgr(0, 255, 0).MCvScalar);
+
+
+            CvInvoke.Imshow(win1, img); //Show the image
             CvInvoke.WaitKey(0);  //Wait for the key pressing event
             CvInvoke.DestroyAllWindows(); //Destroy all windows if key is pressed
     }
